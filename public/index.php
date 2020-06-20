ae12c1e285abae9a5bdee1e78d99dea4a9900948
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-06-20 04:40:08 -0300

    :tada: Initial structure for api

--- public/index.php
+++ public/index.php
@@ -1,17 +1,13 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
+require __DIR__ . "/../config/settings.php";
+require __DIR__ . "/../config/container.php";
+require __DIR__ . "/../config/routes.php";
 
 $app->run();
